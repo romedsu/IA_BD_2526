2cf30184f3ae5992b34f14ec79a8f84a33facee5
romdaw <rom.daw11@gmail.com>
2026-02-17 12:35:19 +0100

    upload u02_python

---
+++ PIA/u02_python/01_general/ejercicios_01/02.03.py
@@ -0,0 +1,12 @@
+#EJERCICIO 3
+
+ingreso=float(input("Introduce el ingreso"))
+impuesto=.0
+
+if ingreso <= 85528:
+    impuesto=(ingreso*1.18)-556.2
+else:
+    impuesto=14839.2+((ingreso-85528)*1.32)
+
+
+print(f"El impuesto es {impuesto} €")
