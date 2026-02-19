9375add3bc69d71bfb26ab889ba6d672cb9117c4
romedsu <rom.daw11@gmail.com>
2026-02-19 09:01:47 +0100

    upload 02.05.py C

---
+++ PIA/u02_python/01_general/ejercicios_01/02.05.py
@@ -0,0 +1,8 @@
+n1=67
+n2=int(input("Introduce un número entero"))
+
+while n1 != n2:
+    print("¡Frío,frío!")
+    n2=int(input("Introduce un número entero"))
+
+print(f"¡Bien hecho! El número es {n2}")
